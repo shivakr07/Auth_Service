bfa6aea6f87a026e06a153c2e14176dbd16001e4
evans <kumarshiva0707@gmail.com>
2023-07-22 19:18:37 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
 // const UserRepository = require('./repository/user-repository');
-
+const UserService = require('./services/user-service');
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -15,6 +15,14 @@ const prepareAndStartServer = () => {
     
     app.listen(PORT, async () => {
         console.log(`Server Started on Port : ${PORT}`);
+
+        const service = new UserService();
+        // const newToken = service.createToken({email : 'evans@admin.com', id : 1});
+        // console.log('new token is', newToken);
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImV2YW5zQGFkbWluLmNvbSIsImlkIjoxLCJpYXQiOjE2OTAwMzMyNzksImV4cCI6MTY5MDAzMzI3OX0.Vnbeb5-b1fUbu3veLcVrwtRwzDycgb7tFXI_q2BjbXg';
+        // const response = service.verifyToken(token);
+        // console.log(response);
+
         // const repo = new UserRepository();
         // const response = await repo.getById(2);
         // console.log(response);
