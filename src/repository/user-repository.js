3e8ff7b3fcd1aeb807321743e6f4b4fbdc8916f2
evans <kumarshiva0707@gmail.com>
2023-08-05 17:38:56 +0530

    done with the Auth service for now

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,67 +1,73 @@
-const { User, Role }  = require('../models/index'); 
+const { User, Role } = require('../models/index');
+const ValidationError = require('../utils/validation-error');
 
 class UserRepository {
-    async create(data) { 
-        try{
+
+    async create(data) {
+        try {
             const user = await User.create(data);
             return user;
-        } catch(error){
-            console.log('something went wrong at the repository layer');
+        } catch (error) {
+            if(error.name == 'SequelizeValidationError') {
+                throw new ValidationError(error);
+            }
+            console.log("Something went wrong on repository layer");
             throw error;
         }
     }
-    async desroy(userId){
-        try{
+
+    async destroy(userId) {
+        try {
             await User.destroy({
-                where:{
-                    id : userId
+                where: {
+                    id: userId
                 }
             });
             return true;
-        } catch(error){
-            console.log('something went wrong at the repository layer');
-            throw error;
-        }
-    }
-    async getById(userId){
-        try{
-            const user = await User.findByPk(userId,{
-                attributes : ['email','id']
-            });
-            return user;
-        }catch(error){
-            console.log('sonmehting went wront at the repo layer')
-        }
-    }
-    async getByEmail(userEmail) {
-        try{    
-            const user = await User.findOne({
-                where : {
-                    email : userEmail
-                }
-            })
-            return user;
-        }catch(error){
-            console.log('Something went wrong in the repository layer');
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
             throw error;
         }
     }
 
-    async isAdmin(userId){
-        try{
-            const user = await User.findByPk(userId);
-            const adminRole = await Role.findOne({
-                where : {
-                    name : 'ADMIN'
-                }
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
             });
-            // console.log(user)
-            return user.hasRole(adminRole);
-        } catch(error){
-            console.log("something went wrong at the repository layer");
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
             throw error;
         }
-    } 
-    
+    }
+
+    async getByEmail(userEmail) {
+        try {
+            const user = await User.findOne({where: {
+                email: userEmail
+            }});
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
+
+    async isAdmin(userId) {
+        try {
+            const user = await User.findByPk(userId);
+            const adminRole = await Role.findOne({
+                where: {
+                    name: 'ADMIN'
+                }
+            });
+            return user.hasRole(adminRole);
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
 }
+
 module.exports = UserRepository;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,99 +1,102 @@
-const UserRepository = require('../repository/user-repository');
 const jwt = require('jsonwebtoken');
-const {JWT_KEY} = require('../config/serverConfig');
 const bcrypt = require('bcrypt');
 
-class UserService{
-    constructor(){
+const UserRepository = require('../repository/user-repository');
+const { JWT_KEY } = require('../config/serverConfig');
+const AppErrors = require('../utils/error-handler');
+
+class UserService {
+    constructor() {
         this.userRepository = new UserRepository();
     }
 
-    async create(data){
-        try{
+    async create(data) {
+        try {
             const user = await this.userRepository.create(data);
-            return user
-        } catch(error){
-            console.log('something went wrong in the service layer');
+            return user;
+        } catch (error) {
+            if(error.name == 'SequelizeValidationError') {
+                throw error;
+            }
+            console.log("Something went wrong in the service layer");
             throw error;
         }
     }
 
-    async signIn(email, plainPassword){
-        try{
-            // step1 -> fetch user using email
+    async signIn(email, plainPassword) {
+        try {
+            // step 1-> fetch the user using the email
             const user = await this.userRepository.getByEmail(email);
-            // step2 -> compare incoming plainPassword to encryptedPassword
+            // step 2-> compare incoming plain password with stores encrypted password
             const passwordsMatch = this.checkPassword(plainPassword, user.password);
 
-            if(!passwordsMatch){
-                console.log('Password doesnt match');
-                throw {error : 'Incorrect password'};
+            if(!passwordsMatch) {
+                console.log("Password doesn't match");
+                throw {error: 'Incorrect password'};
             }
-            // step3 -> if pw match create token and send it to the user
-            const newKJWT = this.createToken({email : user.email, id : user.id});
-            return newKJWT;
-
-        }catch(error){
-            console.log('something went wrong in the sign in process');
+            // step 3-> if passwords match then create a token and send it to the user
+            const newJWT = this.createToken({email: user.email, id: user.id});
+            return newJWT;
+        } catch (error) {
+            console.log("Something went wrong in the sign in process");
             throw error;
         }
     }
-    async isAuthenticated(token){
-        try{
+
+    async isAuthenticated(token) {
+        try {
             const response = this.verifyToken(token);
-            if (!response){
-                throw {error : 'Invalid token'}
+            if(!response) {
+                throw {error: 'Invalid token'}
             }
             const user = await this.userRepository.getById(response.id);
-            if(!user){
-                throw  {error : 'NO user with the corresponding token exists'};
+            if(!user) {
+                throw {error: 'No user with the corresponding token exists'};
             }
             return user.id;
-        }catch(error){
-            console.log('Something went wrong in the auth proccess');
+        } catch (error) {
+            console.log("Something went wrong in the auth process");
             throw error;
         }
     }
 
-
-
     createToken(user) {
-        try{
-            const result = jwt.sign(user,JWT_KEY,{expiresIn:120});
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
             return result;
-        }catch{
-            console.log('something went wrong in token creation');
+        } catch (error) {
+            console.log("Something went wrong in token creation");
             throw error;
         }
     }
 
     verifyToken(token) {
-        try{
+        try {
             const response = jwt.verify(token, JWT_KEY);
             return response;
-        }catch(error){
-            console.log('something went wrong in token validation', error);
+        } catch (error) {
+            console.log("Something went wrong in token validation", error);
             throw error;
         }
     }
 
-    checkPassword(userInputPlainPassword, encryptedPassword){
-        try{
-            return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
-        } catch(error){
-            console.log('Something went wrong in the comparison');
+    checkPassword(userInputPlainPassword, encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password comparison");
             throw error;
         }
     }
 
     isAdmin(userId) {
-        try{
+        try {
             return this.userRepository.isAdmin(userId);
-        } catch(error){
+        } catch (error) {
             console.log("Something went wrong in service layer");
             throw error;
         }
     }
-
 }
+
 module.exports = UserService;
